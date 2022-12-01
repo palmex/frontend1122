63f1793161b90d55ff410b6e00ca0a1ea884f34d
Admin Server <apostlma@ucsd.edu>
2022-12-01 15:00:21 +0000

    update font color/size

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text style={styles.text}>Welcome Stellantis Class of OU Module 3!</Text>
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -17,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color: "blue",
+    fontSize: "24px"
+  }
 });
