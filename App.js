1099637649285c2890cd5fd8c321c99c09347b13
Admin Server <apostlma@ucsd.edu>
2022-12-14 12:50:32 +0000

    added blank cars file

--- App.js
+++ App.js
@@ -2,14 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  let stelVar = "Module 4"
+  console.log(stelVar)
+
   return (
     <View style={styles.container}>
-
-      <Text style={styles.text}>Welcome Stellantis Class of OU Module 3!</Text>
+{/* testnet-api.stellantis-oakland.com/cars/new */}
+      <Text style={styles.text}>Welcome Stellantis Class of OU {stelVar}!</Text>
+      <Text style={styles.text2}>Component Text 2</Text>
       <StatusBar style="auto" />
 
     </View>
   );
+
+
 }
 
 const styles = StyleSheet.create({
@@ -20,7 +27,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text:{
+    color: "#c7971e",
+    backgroundColor:"black",
+    borderColor: "#c7971e",
+    padding: "5%",
+    margin: "5%",
+    borderStyle: "solid",
+    borderWidth: "1px",
+    fontSize: "24px"
+  },
+  text2:{
     color: "blue",
+    backgroundColor:"white",
+    borderColor: "blue",
+    padding: "5%",
+    margin: "5%",
+    borderStyle: "solid",
+    borderWidth: "1px",
     fontSize: "24px"
   }
 });
